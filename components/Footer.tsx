877410952b0193b3cba9736f02db42882cd5260f
jayeshrajani <rajani.jass@gmail.com>
2024-08-15 15:09:40 +0530

    fixed issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,12 +12,13 @@ const Footer = () => {
 
         <div className='flex flex-col items-center text-center'>
             <h1 className='heading lg:max-w-[45vw]'>
-              Let's Create Something Unique <span className='text-cyan-300'>Together</span>
-            </h1>
-            <p className='mt-4 mb-4'>
-              Passionate about tech, fitness, and creativity, I'm always pushing the boundaries of what's possible. <br />
-              If you think my skills align with something you're working on, don't hesitate to reach out!
-            </p>
+            
+  Let&apos;s Create Something Unique <span className='text-cyan-300'>Together</span>
+</h1>
+<p className='mt-4 mb-4'>
+  Passionate about tech, fitness, and creativity, I&apos;m always pushing the boundaries of what&apos;s possible. <br />
+  If you think my skills align with something you&apos;re working on, don&apos;t hesitate to reach out!
+</p>
             <a href="mailto:rajani.jass@gmail.com">
                 <MagicButton
                 title="Let's get in touch"
@@ -33,7 +34,7 @@ const Footer = () => {
             <div className='flex items-center md:gap-3 gap-6'>
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop:blur-lg saturate-180 bg-opacity-75 bg-black-200'>
-                        <img src={profile.img} alt={profile.id} width={20} height={20} />
+                        <img src={profile.img} width={20} height={20} />
                     </div>
                 ))}
             </div>
